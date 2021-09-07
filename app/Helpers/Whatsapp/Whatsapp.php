b6dea37d731424f7255434fce038b23916316491
ersa <ersatama@gmail.com>
2021-09-07 19:23:03 +0600

    whatsapp notification

--- app/Helpers/Whatsapp/Whatsapp.php
+++ app/Helpers/Whatsapp/Whatsapp.php
@@ -9,13 +9,19 @@ class Whatsapp
 
     public function send($data)
     {
-        $url      =   'https://api.chat-api.com/instance'.$this->instance.'/message?token='.$this->token;
-        $options = stream_context_create(['http' => [
-            'method'  => 'POST',
-            'header'  => 'Content-type: application/json',
-            'content' => json_encode($data)
-        ]
-        ]);
-        $result = file_get_contents($url, false, $options);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.chat-api.com/instance'.$this->instance.'/message?token='.$this->token,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $data,
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
     }
 }
